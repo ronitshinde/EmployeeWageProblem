3fbb616a12cce31f3a36b459c5b79ae678581ced
ronitshinde <rony.shinde753@gmail.com>
2022-12-17 22:25:49 +0530

    UC_3 added

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome to Employee Wage Computation Problem");
-
-            EmployeeCheck.EmployeeAttendance();
-        
+            
+            PartTime.EmpPartTime();
         
         
         }
