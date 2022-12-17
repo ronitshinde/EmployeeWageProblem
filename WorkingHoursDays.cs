264537fde023f18707ea5e6e1ccc46a1fb30bd18
ronitshinde <rony.shinde753@gmail.com>
2022-12-18 00:11:19 +0530

    Monthly or houly wages calculated

---
+++ WorkingHoursDays.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class WorkingHoursDays
+    {
+        public const int Is_Full_Time = 1;
+        public const int Is_Part_Time = 2;
+        public const int Emp_Rate_Per_Hour = 20;
+        public const int Num_Of_Working_Days = 20;
+        public const int Max_Hours_In_Month = 100;
+
+
+        public static void HoursDays()
+        {
+
+            int Empwage;
+            int Emphours;
+            int TotalEmpwage = 0;
+            int TotalEmphours = 0;
+            int Totalworkingdays = 0;
+
+            while (TotalEmphours < Max_Hours_In_Month && Totalworkingdays < Num_Of_Working_Days)
+            {
+                Totalworkingdays++;
+                Random random = new Random();
+                int empcheck = random.Next(3);
+
+
+                switch (empcheck)
+                {
+                    case Is_Full_Time:
+                        Emphours = 8;
+                        break;
+
+                    case Is_Part_Time:
+                        Emphours = 4;
+                        break;
+
+                    default:
+                        Emphours = 0;
+                        break;
+                }
+
+                TotalEmphours += Emphours;
+
+                Console.WriteLine("Day # " + Totalworkingdays + "Emp hours " + Emphours);
+            }
+
+            TotalEmpwage = TotalEmphours * Emp_Rate_Per_Hour;
+
+            Console.WriteLine("Tota Emp wage : " + TotalEmpwage);
+
+        }
+
+    }
+}
