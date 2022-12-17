5aab63ed7b89007e8ca5e1eab37dc2fe25c7329e
ronitshinde <rony.shinde753@gmail.com>
2022-12-18 00:05:18 +0530

    UC_5 added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            //MonthlyWages.EmpMonthWages();
+            MonthlyWages.EmpMonthWages();
             //Console.WriteLine("Welcome to Employee Wage Computation Problem");
-            WorkingHoursDays.HoursDays();
+            //WorkingHoursDays.HoursDays();
         }
     }
 }
