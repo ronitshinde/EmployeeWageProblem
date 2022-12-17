e9600c18edc6825699ec1afa89b64a167c51805e
ronitshinde <rony.shinde753@gmail.com>
2022-12-17 17:21:37 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
