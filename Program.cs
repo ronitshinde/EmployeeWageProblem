a32b46499567eda4a763e18c132e2e7cab526994
ronitshinde <rony.shinde753@gmail.com>
2022-12-17 17:52:31 +0530

    UC1 Added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //Console.WriteLine("Welcome to Employee Wage Computation Problem");
+
+            EmployeeCheck.EmployeeAttendance();
+        
+        
+        
         }
     }
 }
